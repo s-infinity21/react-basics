af8d21c3e60200d4bc28971021f79842808a8a75
s-infinity21 <83367007+s-infinity21@users.noreply.github.com>
2021-05-10 22:33:29 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -1,20 +1,22 @@
-import React from "react";
-import Common from "./Common";
+import React from 'react';
+import Common from './Common';
+import img2 from '../src/Images/IMG.png'; //I have entered my profile photo in this section
 
 const About = () => {
-  return (
-    <>
-      <Common
-        text="Curious to know "
-        subhead="A Project Made By Shubh Chaurasia."
-        //imgsrc={img2}
-        imgalt="about.png"
-        link="/contact"
-        headline="About Me"
-        button="Contact Me"
-      />
-    </>
-  );
+	return (
+		<>
+			<Common
+				text='Curious to know '
+				subhead='A Project Made By Shubh Chaurasia.'
+				imgsrc={img2}
+				imgalt='about.png'
+				link='/contact'
+				headline='About Me'
+				button='Contact Me'
+			/>
+		</>
+	);
 };
 
 export default About;
+
