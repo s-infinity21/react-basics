363dfc64844b46d7c6626dc97af5f6a90b9b1e1e
s-infinity21 <83367007+s-infinity21@users.noreply.github.com>
2021-05-14 02:30:36 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react';
 
-const Card = (props) =>{
-    return(
-        <>
-            <div className="col-md-4 col-10 mx-auto">
-                <div class="card">
-                    <img src={props.imgsrc} className="card_image" alt="..." />
-                    <div className="card-body">
-                        <span className="card-title font-weight-bold">{props.title}</span>
-                        <h3 className="card-text">{props.sname}</h3>
-                        <a href={props.link} target="_blank" rel="noreferrer">
-                            <button className="btn_style"> Watch Now </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+const Card = props => {
+	return (
+		<>
+			<div className='col-lg-4 mx-auto'>
+				<div class='card'>
+					<img src={props.imgsrc} className='card_image' alt='...' />
+					<div className='card-body'>
+						<span className='card-title font-weight-bold'>{props.title}</span>
+						<h3 className='card-text'>{props.sname}</h3>
+						<a href={props.link} target='_blank' rel='noreferrer'>
+							<button className='btn_style'>Watch Now </button>
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Card;
